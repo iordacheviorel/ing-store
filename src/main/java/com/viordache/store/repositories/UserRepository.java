b81580cb78a3efcfce4bf90a7ede64acae6ba540
Viorel Iordache <iordacheviorel99@gmail.com>
2024-09-07 14:22:17 +0300

    feat: Add user entity and check that DB table is created

---
+++ src/main/java/com/viordache/store/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.viordache.store.repositories;
+
+import com.viordache.store.entities.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
